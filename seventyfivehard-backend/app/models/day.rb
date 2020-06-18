b101596e37050d4fe2d6953369e1b37d33a72059
helloimiskra <helloimiskra@gmail.com>
2020-06-19 01:27:51 +0900

    adds method to day model

--- seventyfivehard-backend/app/models/day.rb
+++ seventyfivehard-backend/app/models/day.rb
@@ -1,4 +1,11 @@
 class Day < ApplicationRecord
     belongs_to :attempt
+
+    def update_day_complete
+        if self.diet && self.workout_one && self.workout_two && self.no_alcohol &&  self.no_cheatmeal && self.progress_pic && self.drank_water && self.book_read === true
+            self.day_complete = true
+            self.save
+    end
+
     
 end
