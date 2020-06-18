755274ed234e06f40e9b3d9fd1d3375b7e51b6df
helloimiskra <helloimiskra@gmail.com>
2020-06-19 01:29:56 +0900

    adds method to  attempt model

--- seventyfivehard-backend/app/models/attempt.rb
+++ seventyfivehard-backend/app/models/attempt.rb
@@ -1,4 +1,12 @@
 class Attempt < ApplicationRecord
     has_many :days
     validates :startdate, presence: true
+
+    def attempt_successful
+        if self.days.length === 75
+            self.completed = true
+            self.save
+        end
+    end
+
 end
