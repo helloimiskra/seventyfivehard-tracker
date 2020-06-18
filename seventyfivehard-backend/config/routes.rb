d4ee43dfe17da49b0fd9a133e56462991ca1117e
helloimiskra <helloimiskra@gmail.com>
2020-06-19 02:18:56 +0900

    creates nested routes

--- seventyfivehard-backend/config/routes.rb
+++ seventyfivehard-backend/config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      resources :days
-      resources :attempts
-      resources :users
+      resources :users do 
+        resources :attempts do
+          resources :days
+        end
+      end
     end
   end
 end
