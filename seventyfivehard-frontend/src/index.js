2cbdc1d9a4e028096edc2e6456713092caa5b06c
helloimiskra <helloimiskra@gmail.com>
2020-06-19 02:59:21 +0900

    adds correct  thunk and redux npms and sets up basic connection w/ thunk & redux for later on

--- seventyfivehard-frontend/src/index.js
+++ seventyfivehard-frontend/src/index.js
@@ -4,10 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//import { Provider } from 'react-redux'
+//import {createStore, compose, applyMiddleware } from 'redux'
+//import thunk from 'redux-thunk'
+
+//const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+//const store = createStore(combinedReducer, composeEnhancer(applyMiddleware(thunk)));
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  //<Provider store={store}>
+    <App />,
+  //</Provider>,
   document.getElementById('root')
 );
 
