d3f4071d9a1b0763e9bb5cff3b6ee6e1a72851ff
helloimiskra <helloimiskra@gmail.com>
2020-06-23 00:11:44 +0900

    adjusts serializers to show associations in json

--- seventyfivehard-backend/app/serializers/attempt_serializer.rb
+++ seventyfivehard-backend/app/serializers/attempt_serializer.rb
@@ -1,6 +1,6 @@
 class AttemptSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :startdate, :user_id, :completed, :id, :created_at
+  attributes :startdate, :user_id, :completed, :id, :created_at, :days
 
   belongs_to :user
   has_many :days
